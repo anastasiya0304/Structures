f7bf3de4aed3e966e49d6134662215f473e1de91
anastasiya0304 <89684777770@mail.ru>
2017-12-11 23:18:33 +0300

    Update Skiplist.cpp

--- Skiplist.cpp
+++ Skiplist.cpp
@@ -6,13 +6,10 @@
 #include <bits/stdc++.h>
 
 
-// Class to implement node
 class Node
 {
 public:
     int key;
-
-    // Array to hold pointers to node of different level
     Node **forward;
     Node(int, int);
 };
@@ -20,22 +17,15 @@ public:
 Node::Node(int key, int level)
 {
     this->key = key;
-
-    // Allocate memory to forward
     forward = new Node*[level+1];
-
-    // Fill forward array with 0(NULL)
     std::memset(forward, 0, sizeof(Node*)*(level+1));
 };
 
-// Class for Skip list
+
 class SkipList
 {
 
     int MAXLVL;
-
-    // P is the fraction of the nodes with level
-    // i pointers also having level i+1 pointers
     float P;
 
     int level;
@@ -57,11 +47,11 @@ SkipList::SkipList(int MAXLVL, float P)
     this->P = P;
     level = 0;
 
-    // create header node and initialize key to -1
+   
     header = new Node(-1, MAXLVL);
 };
 
-// create random level for node
+
 int SkipList::randomLevel()
 {
     float r = (float)::rand()/RAND_MAX;
@@ -74,7 +64,7 @@ int SkipList::randomLevel()
     return lvl;
 };
 
-// create new node
+
 Node* SkipList::createNode(int key, int level)
 {
     Node *n = new Node(key, level);
@@ -86,16 +76,9 @@ void SkipList::insertElement(int key)
 {
     Node *current = header;
 
-    // create update array and initialize it
     Node *update[MAXLVL+1];
     memset(update, 0, sizeof(Node*)*(MAXLVL+1));
 
-    /*    start from highest level of skip list
-        move the current pointer forward while key
-        is greater than key of node next to current
-        Otherwise inserted current in update and
-        move one level down and continue search
-    */
     for(int i = level; i >= 0; i--)
     {
         while(current->forward[i] != NULL &&
@@ -104,38 +87,22 @@ void SkipList::insertElement(int key)
         update[i] = current;
     }
 
-    /* reached level 0 and forward pointer to
-       right, which is desired position to
-       insert key.
-    */
     current = current->forward[0];
 
-    /* if current is NULL that means we have reached
-       to end of the level or current's key is not equal
-       to key to insert that means we have to insert
-       node between update[0] and current node */
     if (current == NULL || current->key != key)
     {
-        // Generate a random level for node
         int rlevel = randomLevel();
 
-        /* If random level is greater than list's current
-           level (node with highest level inserted in
-           list so far), initialize update value with pointer
-           to header for further use */
         if(rlevel > level)
         {
             for(int i=level+1;i<rlevel+1;i++)
                 update[i] = header;
 
-            // Update the list current level
             level = rlevel;
         }
 
-        // create new node with random level generated
         Node* n = createNode(key, rlevel);
 
-        // insert node by rearranging pointers
         for(int i=0;i<=rlevel;i++)
         {
             n->forward[i] = update[i]->forward[i];
@@ -150,16 +117,9 @@ void SkipList::deleteElement(int key)
 {
     Node *current = header;
 
-    // create update array and initialize it
     Node *update[MAXLVL+1];
     memset(update, 0, sizeof(Node*)*(MAXLVL+1));
 
-    /*    start from highest level of skip list
-        move the current pointer forward while key
-        is greater than key of node next to current
-        Otherwise inserted current in update and
-        move one level down and continue search
-    */
     for(int i = level; i >= 0; i--)
     {
         while(current->forward[i] != NULL  &&
@@ -168,28 +128,20 @@ void SkipList::deleteElement(int key)
         update[i] = current;
     }
 
-    /* reached level 0 and forward pointer to
-       right, which is possibly our desired node.*/
     current = current->forward[0];
 
-    // If current node is target node
     if(current != NULL and current->key == key)
     {
-        /* start from lowest level and rearrange
-           pointers just like we do in singly linked list
-           to remove target node */
+
         for(int i=0;i<=level;i++)
         {
-            /* If at level i, next node is not target
-               node, break the loop, no need to move
-              further level */
+            
             if(update[i]->forward[i] != current)
                 break;
 
             update[i]->forward[i] = current->forward[i];
         }
 
-        // Remove levels having no elements
         while(level>0 &&
               header->forward[level] == 0)
             level--;
@@ -197,17 +149,11 @@ void SkipList::deleteElement(int key)
     }
 };
 
-// Search for element in skip list
+
 void SkipList::searchElement(int key)
 {
     Node *current = header;
 
-    /*    start from highest level of skip list
-        move the current pointer forward while key
-        is greater than key of node next to current
-        Otherwise inserted current in update and
-        move one level down and continue search
-    */
     for(int i = level; i >= 0; i--)
     {
         while(current->forward[i] &&
@@ -216,12 +162,8 @@ void SkipList::searchElement(int key)
 
     }
 
-    /* reached level 0 and advance pointer to
-       right, which is possibly our desired node*/
     current = current->forward[0];
 
-    // If current node have key equal to
-    // search key, we have found our target node
     if(current and current->key == key)
         std::cout<<"Found key: "<<key<<"\n";
 };
